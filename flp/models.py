60376b88946d09375cf673febd36d1fb52dcc486
Stuart Langridge <sil@kryogenix.org>
2014-09-05 18:07:37 +0100

    Longer URLs

--- flp/models.py
+++ flp/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
     date = models.DateTimeField()
     length = models.IntegerField()
     post_id = models.CharField(max_length=1000)
-    link = models.URLField()
+    link = models.URLField(max_length=1000)
     author = models.CharField(max_length=255, null=True)
     def __unicode__(self): return u'Post %s' % (self.link,)
 
@@ -23,7 +23,7 @@ class Score(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     month = models.IntegerField() # stored so we can group on it because
     year = models.IntegerField()  # group by datepart() won't work across DBs
-    attached_url = models.URLField()
+    attached_url = models.URLField(max_length=1000)
     def __unicode__(self): return u'Score %s ("%s") on %s' % (self.value, self.reason, self.post)
 
 class User2Score(models.Model):
